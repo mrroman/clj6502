258035546fe49070b31449959801896c2f1b55a4
Konrad Mrożek <konrad.mrozek@gmail.com>
2013-06-17 23:41:02 +0200

    Refactoring absolute. Introduced absolute? and absolute-idx?

--- src/clj6502/mnemonics.clj
+++ src/clj6502/mnemonics.clj
@@ -12,28 +12,32 @@
                         [code-zero x]
                         [code (lsb x) (msb x)])))
 
+(def absolute? number?)
+(defn absolute-idx? [x y idx]
+  (and (number? x) (= idx y)))
+
 (defn lda
   ([x]
-   (cond (number? x) (absolute 173 165 x)
+   (cond (absolute? x) (absolute 173 165 x)
          (keyword? x) (immediate 169 x)
          (vector? x) [161 (first x)]))
   ([x y]
-   (cond (and (number? x) (= :x y)) (absolute 189 181 x)
-         (and (number? x) (= :y y)) (absolute 185 x)
+   (cond (absolute-idx? x y :x) (absolute 189 181 x)
+         (absolute-idx? x y :y) (absolute 185 x)
          (vector? x) [177 (first x)])))
 
 (defn ldx
   ([x]
    (cond (keyword? x) (immediate 162 x)
-         (number? x) (absolute 174 166 x)))
+         (absolute? x) (absolute 174 166 x)))
   ([x y]
-   (cond (and (number? x) (= :y y)) (absolute 190 182 x))))
+   (cond (absolute-idx? x y :y) (absolute 190 182 x))))
 
 (defn ldy
   ([x]
    (cond (keyword? x) (immediate 160 x)
          (number? x) (absolute 172 164 x)))
   ([x y]
-   (cond (and (number? x) (= :x y)) (absolute 188 180 x))))
+   (cond (absolute-idx? x y :x) (absolute 188 180 x))))
 
 
