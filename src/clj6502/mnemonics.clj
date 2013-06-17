12f872c84d0f326391e0934c235f21614b08b645
Konrad Mrożek <konrad.mrozek@gmail.com>
2013-06-17 23:31:15 +0200

    Implemented LDX and LDA

--- src/clj6502/mnemonics.clj
+++ src/clj6502/mnemonics.clj
@@ -7,18 +7,24 @@
   [code (Integer/parseInt (name x))])
 
 (defn absolute 
+  ([code x] [code (lsb x) (msb x)])
   ([code code-zero x] (if (< x 256)
                         [code-zero x]
                         [code (lsb x) (msb x)])))
 
-(defn lda 
+(defn lda
   ([x]
    (cond (number? x) (absolute 173 165 x)
          (keyword? x) (immediate 169 x)
          (vector? x) [161 (first x)]))
   ([x y]
-   (cond (number? x) (absolute 189 181 x)
+   (cond (and (number? x) (= :x y)) (absolute 189 181 x)
+         (and (number? x) (= :y y)) (absolute 185 x)
          (vector? x) [177 (first x)])))
 
-(defn ldx [x]
-  (cond (keyword? x) (immediate 162 x)))
+(defn ldx 
+  ([x]
+   (cond (keyword? x) (immediate 162 x)
+             (number? x) (absolute 174 166 x)))
+  ([x y]
+   (cond (and (number? x) (= :y y)) (absolute 190 182 x))))
