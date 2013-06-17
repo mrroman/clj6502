57336465b3ad6efd13be5c5d5c08a1ce5d64f2a4
Konrad Mrożek <konrad.mrozek@gmail.com>
2013-06-17 23:18:33 +0200

    Refactoring. Introduce absolute function

--- src/clj6502/mnemonics.clj
+++ src/clj6502/mnemonics.clj
@@ -6,17 +6,18 @@
 (defn immediate [code x]
   [code (Integer/parseInt (name x))])
 
+(defn absolute 
+  ([code code-zero x] (if (< x 256)
+                        [code-zero x]
+                        [code (lsb x) (msb x)])))
+
 (defn lda 
   ([x]
-   (cond (number? x) (if (< x 256) 
-                       [165 x]
-                       [173 (lsb x) (msb x)])
+   (cond (number? x) (absolute 173 165 x)
          (keyword? x) (immediate 169 x)
          (vector? x) [161 (first x)]))
   ([x y]
-   (cond (number? x) (if (< x 256)
-                       [181 x]
-                       [189 (lsb x) (msb x)])
+   (cond (number? x) (absolute 189 181 x)
          (vector? x) [177 (first x)])))
 
 (defn ldx [x]
