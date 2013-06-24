7d54cc3f69e15001a5c9dcb31fa45b8f53e6daf2
Konrad Mrożek <konrad.mrozek@gmail.com>
2013-06-24 23:10:17 +0200

    Added almost every mnemonic. Need to add few tests for mnemonics.

--- spec/clj6502/mnemonics_spec.clj
+++ spec/clj6502/mnemonics_spec.clj
@@ -11,30 +11,241 @@
     `(describe (:name (meta (var ~m-func)))
                ~@it-rules)))
 
+(describe-mnemonic adc
+                  [[:44]        [(hex :69) 44]]
+                  [[44]         [(hex :65) 44]]
+                  [[44 :x]      [(hex :75) 44]]
+                  [[710]        [(hex :6D) 198 2]]
+                  [[710 :x]     [(hex :7D) 198 2]]
+                  [[710 :y]     [(hex :79) 198 2]]
+                  [[[44 :x]]    [(hex :61) 44]]
+                  [[[44] :y]    [(hex :71) 44]]
+                 )
+
+(describe-mnemonic and
+                  [[:44]        [(hex :29) 44]]
+                  [[44]         [(hex :25) 44]]
+                  [[44 :x]      [(hex :35) 44]]
+                  [[710]        [(hex :2D) 198 2]]
+                  [[710 :x]     [(hex :3D) 198 2]]
+                  [[710 :y]     [(hex :39) 198 2]]
+                  [[[44 :x]]    [(hex :21) 44]]
+                  [[[44] :y]   [(hex :31) 44]]
+                 )
+
+(describe-mnemonic asl
+                  [[:a]         [(hex :0A)]]
+                  [[44]         [(hex :06) 44]]
+                  [[44 :x]      [(hex :16) 44]]
+                  [[710]        [(hex :0E) 198 2]]
+                  [[710 :x]     [(hex :1E) 198 2]]
+                 )
+
+(describe-mnemonic bit
+                  [[44]         [(hex :24) 44]]
+                  [[710]        [(hex :2C) 198 2]]
+                 )
+
+(describe-mnemonic brk
+                  [[]           [(hex :00)]]
+                 )
+
+(describe-mnemonic cmp
+                  [[:44]        [(hex :C9) 44]]
+                  [[44]         [(hex :C5) 44]]
+                  [[44 :x]      [(hex :D5) 44]]
+                  [[710]        [(hex :CD) 198 2]]
+                  [[710 :x]     [(hex :DD) 198 2]]
+                  [[710 :y]     [(hex :D9) 198 2]]
+                  [[[44 :x]]    [(hex :C1) 44]]
+                  [[[44] :y]   [(hex :D1) 44]]
+                 )
+
+(describe-mnemonic cpx
+                  [[:44]        [(hex :E0) 44]]
+                  [[44]         [(hex :E4) 44]]
+                  [[710]        [(hex :EC) 198 2]]
+                 )
+
+(describe-mnemonic cpy
+                  [[:44]        [(hex :C0) 44]]
+                  [[44]         [(hex :C4) 44]]
+                  [[710]        [(hex :CC) 198 2]]
+                 )
+
+(describe-mnemonic dec
+                  [[44]         [(hex :C6) 44]]
+                  [[44 :x]      [(hex :D6) 44]]
+                  [[710]        [(hex :CE) 198 2]]
+                  [[710 :x]     [(hex :DE) 198 2]]
+                 )
+
+(describe-mnemonic eor
+                  [[:44]        [(hex :49) 44]]
+                  [[44]         [(hex :45) 44]]
+                  [[44 :x]      [(hex :55) 44]]
+                  [[710]        [(hex :4D) 198 2]]
+                  [[710 :x]     [(hex :5D) 198 2]]
+                  [[710 :y]     [(hex :59) 198 2]]
+                  [[[44 :x]]    [(hex :41) 44]]
+                  [[[44] :y]   [(hex :51) 44]]
+                 )
+
+(describe-mnemonic inc
+                  [[44]         [(hex :E6) 44]]
+                  [[44 :x]      [(hex :F6) 44]]
+                  [[710]        [(hex :EE) 198 2]]
+                  [[710 :x]     [(hex :FE) 198 2]]
+                 )
+
+(describe-mnemonic jmp
+                  [[710]        [(hex :4C) 198 2]]
+                  [[[44]]      [(hex :6C) 44]]
+                 )
+
+(describe-mnemonic jsr
+                  [[710]        [(hex :20) 198 2]]
+                 )
+
 (describe-mnemonic lda
-                   [[0]       [(hex :A5) 0]]
-                   [[512]     [(hex :AD) 0 2]]
-                   [[710]     [(hex :AD) 198 2]]
-                   [[:10]     [(hex :A9) 10]]
-                   [[0 :x]    [(hex :B5) 0]]
-                   [[128 :x]  [(hex :B5) 128]]
-                   [[256 :x]  [(hex :BD) 0 1]]
-                   [[0 :y]    [(hex :B9) 0 0]]
-                   [[[0 :x]]  [(hex :A1) 0]]
-                   [[[0] :y]  [(hex :B1) 0]])
+                  [[:44]        [(hex :A9) 44]]
+                  [[44]         [(hex :A5) 44]]
+                  [[44 :x]      [(hex :B5) 44]]
+                  [[710]        [(hex :AD) 198 2]]
+                  [[710 :x]     [(hex :BD) 198 2]]
+                  [[710 :y]     [(hex :B9) 198 2]]
+                  [[[44 :x]]    [(hex :A1) 44]]
+                  [[[44] :y]   [(hex :B1) 44]]
+                 )
 
 (describe-mnemonic ldx
-                   [[:10]     [(hex :A2) 10]]
-                   [[:12]     [(hex :A2) 12]]
-                   [[44]      [(hex :A6) 44]]
-                   [[44 :y]   [(hex :B6) 44]]
-                   [[513]     [(hex :AE) 1 2]]
-                   [[513 :y]  [(hex :BE) 1 2]])
+                  [[:44]        [(hex :A2) 44]]
+                  [[44]         [(hex :A6) 44]]
+                  [[710]        [(hex :AE) 198 2]]
+                  [[710 :y]     [(hex :BE) 198 2]]
+                 )
 
 (describe-mnemonic ldy
-                   [[:10]     [(hex :A0) 10]]
-                   [[:12]     [(hex :A0) 12]]
-                   [[44]      [(hex :A4) 44]]
-                   [[44 :x]   [(hex :B4) 44]]
-                   [[513]     [(hex :AC) 1 2]]
-                   [[513 :x]  [(hex :BC) 1 2]])
+                  [[:44]        [(hex :A0) 44]]
+                  [[44]         [(hex :A4) 44]]
+                  [[44 :x]      [(hex :B4) 44]]
+                  [[710]        [(hex :AC) 198 2]]
+                  [[710 :x]     [(hex :BC) 198 2]]
+                 )
+
+(describe-mnemonic lsr
+                  [[:a]         [(hex :4A)]]
+                  [[44]         [(hex :46) 44]]
+                  [[44 :x]      [(hex :56) 44]]
+                  [[710]        [(hex :4E) 198 2]]
+                  [[710 :x]     [(hex :5E) 198 2]]
+                 )
+
+(describe-mnemonic nop
+                  [[]           [(hex :EA)]]
+                 )
+
+(describe-mnemonic ora
+                  [[:44]        [(hex :09) 44]]
+                  [[44]         [(hex :05) 44]]
+                  [[44 :x]      [(hex :15) 44]]
+                  [[710]        [(hex :0D) 198 2]]
+                  [[710 :x]     [(hex :1D) 198 2]]
+                  [[710 :y]     [(hex :19) 198 2]]
+                  [[[44 :x]]    [(hex :01) 44]]
+                  [[[44] :y]   [(hex :11) 44]]
+                 )
+
+(describe-mnemonic rol
+                  [[:a]         [(hex :2A)]]
+                  [[44]         [(hex :26) 44]]
+                  [[44 :x]      [(hex :36) 44]]
+                  [[710]        [(hex :2E) 198 2]]
+                  [[710 :x]     [(hex :3E) 198 2]]
+                 )
+
+(describe-mnemonic ror
+                  [[:a]         [(hex :6A)]]
+                  [[44]         [(hex :66) 44]]
+                  [[44 :x]      [(hex :76) 44]]
+                  [[710]        [(hex :6E) 198 2]]
+                  [[710 :x]     [(hex :7E) 198 2]]
+                 )
+
+(describe-mnemonic rti
+                  [[]           [(hex :40)]]
+                 )
+
+(describe-mnemonic rts
+                  [[]           [(hex :60)]]
+                 )
+
+(describe-mnemonic sbc
+                  [[:44]        [(hex :E9) 44]]
+                  [[44]         [(hex :E5) 44]]
+                  [[44 :x]      [(hex :F5) 44]]
+                  [[710]        [(hex :ED) 198 2]]
+                  [[710 :x]     [(hex :FD) 198 2]]
+                  [[710 :y]     [(hex :F9) 198 2]]
+                  [[[44 :x]]    [(hex :E1) 44]]
+                  [[[44] :y]   [(hex :F1) 44]]
+                 )
+
+(describe-mnemonic sta
+                  [[44]         [(hex :85) 44]]
+                  [[44 :x]      [(hex :95) 44]]
+                  [[710]        [(hex :8D) 198 2]]
+                  [[710 :x]     [(hex :9D) 198 2]]
+                  [[710 :y]     [(hex :99) 198 2]]
+                  [[[44 :x]]    [(hex :81) 44]]
+                  [[[44] :y]   [(hex :91) 44]]
+                 )
+
+(describe-mnemonic stx
+                  [[44]         [(hex :86) 44]]
+                  [[710]        [(hex :8E) 198 2]]
+                 )
+
+(describe-mnemonic sty
+                  [[44]         [(hex :84) 44]]
+                  [[44 :x]      [(hex :94) 44]]
+                  [[710]        [(hex :8C) 198 2]]
+                 )
+;; (describe-mnemonic adc
+;;                   [[:44]     [(hex :69) 44]]
+;;                   [[44]      [(hex :65) 44]]
+;;                   [[44 :x]   [(hex :75) 44]]
+;;                   [[710]     [(hex :6D) 198 2]]
+;;                   [[710 :x]  [(hex :7D) 198 2]]
+;;                   [[710 :y]  [(hex :79) 198 2]]
+;;                   [[[44 :x]] [(hex :61) 44]]
+;;                   [[[44] :y] [(hex :71) 44]])
+;;
+;;(describe-mnemonic lda
+;;                   [[0]       [(hex :A5) 0]]
+;;                   [[512]     [(hex :AD) 0 2]]
+;;                   [[710]     [(hex :AD) 198 2]]
+;;                   [[:10]     [(hex :A9) 10]]
+;;                   [[0 :x]    [(hex :B5) 0]]
+;;                   [[128 :x]  [(hex :B5) 128]]
+;;                   [[256 :x]  [(hex :BD) 0 1]]
+;;                   [[0 :y]    [(hex :B9) 0 0]]
+;;                   [[[0 :x]]  [(hex :A1) 0]]
+;;                   [[[0] :y]  [(hex :B1) 0]])
+;;
+;;(describe-mnemonic ldx
+;;                   [[:10]     [(hex :A2) 10]]
+;;                   [[:12]     [(hex :A2) 12]]
+;;                   [[44]      [(hex :A6) 44]]
+;;                   [[44 :y]   [(hex :B6) 44]]
+;;                   [[513]     [(hex :AE) 1 2]]
+;;                   [[513 :y]  [(hex :BE) 1 2]])
+;;
+;;(describe-mnemonic ldy
+;;                   [[:10]     [(hex :A0) 10]]
+;;                   [[:12]     [(hex :A0) 12]]
+;;                   [[44]      [(hex :A4) 44]]
+;;                   [[44 :x]   [(hex :B4) 44]]
+;;                   [[513]     [(hex :AC) 1 2]]
+;;                   [[513 :x]  [(hex :BC) 1 2]])
+;;
--- src/clj6502/mnemonics.clj
+++ src/clj6502/mnemonics.clj
@@ -3,41 +3,295 @@
 (defn lsb [x] (mod x 256))
 (defn msb [x] (quot x 256))
 
+(defn- hex [x] (Integer/parseInt (name x) 16))
 (defn immediate [code x]
-  [code (Integer/parseInt (name x))])
+  [(hex code) (Integer/parseInt (name x))])
 
 (defn absolute 
-  ([code x] [code (lsb x) (msb x)])
+  ([code x] [(hex code) (lsb x) (msb x)])
   ([code code-zero x] (if (< x 256)
-                        [code-zero x]
-                        [code (lsb x) (msb x)])))
+                        [(hex code-zero) x]
+                        [(hex code) (lsb x) (msb x)])))
 
 (def absolute? number?)
 (defn absolute-idx? [x y idx]
   (and (number? x) (= idx y)))
 
-(defn lda
-  ([x]
-   (cond (absolute? x) (absolute 173 165 x)
-         (keyword? x) (immediate 169 x)
-         (vector? x) [161 (first x)]))
-  ([x y]
-   (cond (absolute-idx? x y :x) (absolute 189 181 x)
-         (absolute-idx? x y :y) (absolute 185 x)
-         (vector? x) [177 (first x)])))
+(defmacro mnemonic [m-name 
+                    & {:keys [immediate zero-page zero-page-x absolute absolute-x absolute-y indirect indirect-x indirect-y accumulator implied]}]
+  `(defn ~m-name
+     ([]
+      [(hex ~implied)])
+     ([x#]
+      (cond (absolute? x#) (if (nil? ~zero-page)
+                             (absolute ~absolute x#)
+                             (absolute ~absolute ~zero-page x#))
+            (keyword? x#) (if (= x# :a)
+                            [(hex ~accumulator)]
+                            (immediate ~immediate x#))
+            (vector? x#) (if (empty? (rest x#))
+                           [(hex ~indirect) (first x#)]
+                           [(hex ~indirect-x) (first x#)])))
+     ([x# y#]
+      (cond (absolute-idx? x# y# :x) (absolute ~absolute-x ~zero-page-x x#)
+            (absolute-idx? x# y# :y) (absolute ~absolute-y x#)
+            (vector? x#) [(hex ~indirect-y) (first x#)]))))
 
-(defn ldx
-  ([x]
-   (cond (keyword? x) (immediate 162 x)
-         (absolute? x) (absolute 174 166 x)))
-  ([x y]
-   (cond (absolute-idx? x y :y) (absolute 190 182 x))))
+(mnemonic adc
+          :immediate :69
+          :zero-page :65
+          :zero-page-x :75
+          :absolute :6D
+          :absolute-x :7D
+          :absolute-y :79
+          :indirect-x :61
+          :indirect-y :71
+                 )
 
-(defn ldy
-  ([x]
-   (cond (keyword? x) (immediate 160 x)
-         (number? x) (absolute 172 164 x)))
-  ([x y]
-   (cond (absolute-idx? x y :x) (absolute 188 180 x))))
+(mnemonic and
+          :immediate :29
+          :zero-page :25
+          :zero-page-x :35
+          :absolute :2D
+          :absolute-x :3D
+          :absolute-y :39
+          :indirect-x :21
+          :indirect-y :31
+                 )
 
+(mnemonic asl
+          :accumulator :0A
+          :zero-page :06
+          :zero-page-x :16
+          :absolute :0E
+          :absolute-x :1E
+                 )
 
+(mnemonic bit
+          :zero-page :24
+          :absolute :2C
+                 )
+
+(mnemonic bpl :implied :10)
+
+(mnemonic bmi :implied :30)
+
+(mnemonic bvc :implied :50)
+
+(mnemonic bvs :implied :70)
+
+(mnemonic bcc :implied :90)
+
+(mnemonic bcs :implied :B0)
+
+(mnemonic bne :implied :D0)
+
+(mnemonic beq :implied :F0)
+
+(mnemonic brk
+          :implied :00
+                 )
+
+(mnemonic cmp
+          :immediate :C9
+          :zero-page :C5
+          :zero-page-x :D5
+          :absolute :CD
+          :absolute-x :DD
+          :absolute-y :D9
+          :indirect-x :C1
+          :indirect-y :D1
+                 )
+
+(mnemonic cpx
+          :immediate :E0
+          :zero-page :E4
+          :absolute :EC
+                 )
+
+(mnemonic cpy
+          :immediate :C0
+          :zero-page :C4
+          :absolute :CC
+                 )
+
+(mnemonic dec
+          :zero-page :C6
+          :zero-page-x :D6
+          :absolute :CE
+          :absolute-x :DE
+                 )
+
+(mnemonic eor
+          :immediate :49
+          :zero-page :45
+          :zero-page-x :55
+          :absolute :4D
+          :absolute-x :5D
+          :absolute-y :59
+          :indirect-x :41
+          :indirect-y :51
+                 )
+
+(mnemonic clc :implied :18)
+
+(mnemonic sec :implied :38)
+
+(mnemonic cli :implied :58)
+
+(mnemonic sei :implied :78)
+
+(mnemonic clv :implied :B8)
+
+(mnemonic cld :implied :D8)
+
+(mnemonic sed :implied :F8)
+
+(mnemonic inc
+          :zero-page :E6
+          :zero-page-x :F6
+          :absolute :EE
+          :absolute-x :FE
+                 )
+
+(mnemonic jmp
+          :absolute :4C
+          :indirect :6C
+                 )
+
+(mnemonic jsr
+          :absolute :20
+                 )
+
+(mnemonic lda
+          :immediate :A9
+          :zero-page :A5
+          :zero-page-x :B5
+          :absolute :AD
+          :absolute-x :BD
+          :absolute-y :B9
+          :indirect-x :A1
+          :indirect-y :B1
+                 )
+
+(mnemonic ldx
+          :immediate :A2
+          :zero-page :A6
+          :absolute :AE
+          :absolute-y :BE
+                 )
+
+(mnemonic ldy
+          :immediate :A0
+          :zero-page :A4
+          :zero-page-x :B4
+          :absolute :AC
+          :absolute-x :BC
+                 )
+
+(mnemonic lsr
+          :accumulator :4A
+          :zero-page :46
+          :zero-page-x :56
+          :absolute :4E
+          :absolute-x :5E
+                 )
+
+(mnemonic nop
+          :implied :EA
+                 )
+
+(mnemonic ora
+          :immediate :09
+          :zero-page :05
+          :zero-page-x :15
+          :absolute :0D
+          :absolute-x :1D
+          :absolute-y :19
+          :indirect-x :01
+          :indirect-y :11
+                 )
+
+(mnemonic tax :implied :AA)
+
+(mnemonic txa :implied :8A)
+
+(mnemonic dex :implied :CA)
+
+(mnemonic inx :implied :E8)
+
+(mnemonic tay :implied :A8)
+
+(mnemonic tya :implied :98)
+
+(mnemonic dey :implied :88)
+
+(mnemonic iny :implied :C8)
+
+(mnemonic rol
+          :accumulator :2A
+          :zero-page :26
+          :zero-page-x :36
+          :absolute :2E
+          :absolute-x :3E
+                 )
+
+(mnemonic ror
+          :accumulator :6A
+          :zero-page :66
+          :zero-page-x :76
+          :absolute :6E
+          :absolute-x :7E
+                 )
+
+(mnemonic rti
+          :implied :40
+                 )
+
+(mnemonic rts
+          :implied :60
+                 )
+
+(mnemonic sbc
+          :immediate :E9
+          :zero-page :E5
+          :zero-page-x :F5
+          :absolute :ED
+          :absolute-x :FD
+          :absolute-y :F9
+          :indirect-x :E1
+          :indirect-y :F1
+                 )
+
+(mnemonic sta
+          :zero-page :85
+          :zero-page-x :95
+          :absolute :8D
+          :absolute-x :9D
+          :absolute-y :99
+          :indirect-x :81
+          :indirect-y :91
+                 )
+
+(mnemonic txs :implied :9A)
+
+(mnemonic tsx :implied :BA)
+
+(mnemonic pha :implied :48)
+
+(mnemonic pla :implied :68)
+
+(mnemonic php :implied :08)
+
+(mnemonic plp :implied :28)
+
+(mnemonic stx
+          :zero-page :86
+          :absolute :8E
+                 )
+
+(mnemonic sty
+          :zero-page :84
+          :zero-page-x :94
+          :absolute :8C
+                 )
